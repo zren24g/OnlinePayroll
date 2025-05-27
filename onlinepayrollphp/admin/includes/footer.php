6a48230156cc8b72699482287dd5ad748ffcfe08
zren24g <you@example.com>
2025-05-27 20:43:22 +0800

    this is the system

---
+++ onlinepayrollphp/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2020 <a href="https://www.youtube.com/channel/UCsFgC9ggwrmYR2XqEHXpbNg/">Ser Bermz</a></strong>
+</footer>
